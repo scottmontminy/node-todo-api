e4daaf42125db7860c1acaa72d94e8c3c906fe60
Scott Montminy <scott@madeinthestudio.com>
2017-10-11 21:47:39 -0400

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const port = process.env.PORT
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
-  // console.log(req.body)
   var todo = new Todo({
     text: req.body.text
   })
@@ -92,6 +91,20 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo})
   }).catch((e) => {
     res.status(400).send()
+  })
+})
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    console.log('foo')
+    res.status(400).send(e)
   })
 })
 
